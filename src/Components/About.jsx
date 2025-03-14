bf20f5c99aa2657441e9dfe7858668cca0e1b8ef
Prasanth k <prasanth2817@gmail.com>
2025-03-14 15:39:05 +0530

    updated profile image

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,6 +1,4 @@
-import React from "react";
-import Profilephoto1 from "../Images/portfolioPhotoLight.png";
-import Profilephoto2 from "../Images/profile_pic_19_12_Light.jpg"
+import Profilephoto from "../Images/Profile_pic_06_03.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faFileLines } from "@fortawesome/free-solid-svg-icons";
 
@@ -9,7 +7,8 @@ const About = () => {
     <section id="about" className="pt-20 lg:py-12 bg-gray-900">
       <div className="container mx-auto lg:pt-16 lg:px-4">
         <h2 className="text-2xl md:text-4xl xl:text-5xl font-bold text-center xl:my-16 text-gray-100 mb-8">
-          <FontAwesomeIcon icon={faUser} /> About{" "}
+          <FontAwesomeIcon icon={faUser} />
+          About{" "}
           <span className="text-2xl md:text-4xl lg:text-5xl font-extrabold text-orange-500">
             Me
           </span>
@@ -17,7 +16,7 @@ const About = () => {
         <div className="flex flex-col items-center lg:flex-row 1024x1366:flex-col xl:justify-around lg:gap-8 xl:gap-12 gap-8">
           <div className="lg:mb-0">
             <img
-              src={Profilephoto2}
+              src={Profilephoto}
               alt="About Me"
               className="rounded-4xl md:w-9/12 1024x1366:w-9/12 1024x1366:ml-24 md:mx-16 lg:mx-2 lg:mt-2 md:my-8 lg:w-full mix-blend-luminosity transition duration-300 cursor-pointer hover:mix-blend-normal"
             />
@@ -25,18 +24,19 @@ const About = () => {
 
           <div className="">
             <p className="xl:text-2xl lg:text-lg lg:mb-4 1024x1366:text-base md:text-2xl 1024x1366:my-4 text-justify mb-4 text-cyan-600 font-medium">
-              I'm Prasanth K, a Civil Engineering graduate who transitioned to
-              software development through a comprehensive course on Guvi,
+              I&apos;m Prasanth K, a Civil Engineering graduate who transitioned
+              to software development through a comprehensive course on Guvi,
               specializing in MERN stack. Currently seeking entry-level
               opportunities, I have hands-on experience in creating full-stack
               projects and am passionate about innovation and impactful
               contributions. My journey has equipped me with unique
               problem-solving skills and a commitment to continuous learning.
-              Let's connect to discuss how I can contribute to your projects.
+              Let&apos;s connect to discuss how I can contribute to your
+              projects.
             </p>
-            <button className="container mx-auto w-full sm:w-1/2 1024x1366:mt-8 1024x1366:text-3xl md:w-5/12 sm:flex sm:justify-center text-xl md:text-2xl font-bold lg:font-semibold text-gray-50 hover:bg-orange-400 bg-orange-500 rounded-xl px-6 py-2 sm:px-8 md:px-6">
+            <button className="container mx-auto w-full sm:w-1/2 1024x1366:mt-8 1024x1366:text-3xl md:w-5/12 sm:flex sm:justify-center text-xl md:text-2xl font-bold lg:font-semibold text-gray-50 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-orange-400 bg-orange-500 rounded-xl px-6 py-2 sm:px-8 md:px-6">
               <a
-                href="https://drive.google.com/file/d/1zHJm-VHBtzMyH-Q3AxAE2wb1WwKj5_Nj/view?usp=drivesdk"
+                href="https://drive.google.com/file/d/1Eh6U3vXSG8eM-rzvtxW7ZKWpYG3KL-xj/view?usp=drivesdk"
                 target="_blank"
                 rel="noopener noreferrer"
               >
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -21,7 +21,9 @@ function Home() {
       <ParticleBackground id="particles" />
       <Navbar />
       <section
-        className="flex flex-col 1024x1366:mt-12 lg:p-8 justify-around xs:justify-evenly 1024x1366:flex-col 1024x1366:justify-normal 1024x1366:pt-16 relative top-4 lg:top-16 sm:top-4 lg:flex-row py-20 lg:py-0 md:h-screen home" id="home">
+        className="flex flex-col 1024x1366:mt-12 lg:p-8 justify-around xs:justify-evenly 1024x1366:flex-col 1024x1366:justify-normal 1024x1366:pt-16 relative top-4 lg:top-16 sm:top-4 lg:flex-row py-20 lg:py-0 md:h-screen home"
+        id="home"
+      >
         <div className="order-2 1024x1366:order-2 lg:order-1 content py-4 lg:px-4 sm:py-4 md:py-0 relative lg:top-20 lg-xl:top-32 lg:w-full xl:mt-16 z-10">
           <h2 className="text-3xl xs:text-4xl md:text-5xl lg:text-4xl 1024x1366:text-6xl xl:text-6xl font-extrabold tracking-wide text-blue-900">
             Hi There,
@@ -31,49 +33,59 @@ function Home() {
             </span>
           </h2>
           <p className="text-xl sm:text-2xl xs:font-semibold py-4 md:text-3xl 1024x1366:text-5xl xl:text-4xl font-medium text-gray-600">
-            I am Into <span className="text-xl xs:text-2xl sm:text-3xl md:text-5xl lg:text-4xl 1024x1366:text-6xl xl:text-5xl font-bold">{<DynamicText />}</span>
+            I am Into{" "}
+            <span className="text-xl xs:text-2xl sm:text-3xl md:text-5xl lg:text-4xl 1024x1366:text-6xl xl:text-5xl font-bold">
+              {<DynamicText />}
+            </span>
           </p>
-          <div className="btn absolute mt-4 lg:mt-8 px-6 xl:px-8 py-4 xl:py-6 bg-blue-800 text-gray-200 font-bold text-2xl 1024x1366:text-3xl xl:text-3xl rounded-3xl">
+          {/* <div className="btn absolute mt-4 lg:mt-8 px-6 xl:px-8 py-4 xl:py-6 bg-blue-800 text-gray-200 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:gap-12 font-bold text-2xl 1024x1366:text-3xl xl:text-3xl rounded-3xl">
           <a href="#about" >
-            <span>About Me</span>
+            <span>About Me
             <i className="fas fa-arrow-circle-down"></i>
+            </span>
           </a>
+          </div> */}
+          <div className="btn absolute mt-4 lg:mt-8 px-6 xl:px-8 py-4 xl:py-6 bg-blue-800 text-gray-200 transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 font-bold text-2xl 1024x1366:text-3xl xl:text-3xl rounded-3xl group">
+            <a href="#about" className="flex items-center gap-2">
+              <span>About Me</span>
+              <i className="fas fa-arrow-circle-down transition-transform duration-300 ease-in-out group-hover:translate-y-0.5 group-hover:opacity-80"></i>
+            </a>
           </div>
-          <div className="socials relative mt-28 md:mt-36 lg:mt-40 xl:mt-44">
-  <ul className="social-icons space-x-4 p-0 mb-0 list-none">
-    <li className="inline-block mb-6 md:mb-8 lg:mb-10">
-      <a
-        className="linkedin inline-block text-2xl md:text-4xl lg-xl:text-3xl leading-[44px] md:leading-[58px] lg-xl:leading-[52px] text-cyan-400 bg-[#09011b] w-11 h-11 md:w-14 md:h-14 lg-xl:w-12 lg-xl:h-12 text-center mr-2 rounded-full transition-all duration-200 ease-linear hover:text-white hover:bg-[#007bb6]"
-        aria-label="LinkedIn"
-        href="https://www.linkedin.com/in/prasanth2817"
-        target="_blank"
-      >
-        <FontAwesomeIcon icon={faLinkedin} />
-      </a>
-    </li>
-    <li className="inline-block mb-6 md:mb-8 lg:mb-10">
-      <a
-        className="github inline-block text-2xl md:text-4xl leading-[44px] lg-xl:text-3xl lg-xl:leading-[52px] md:leading-[58px] text-cyan-400 bg-[#09011b] w-11 h-11 md:w-14 md:h-14 lg-xl:w-12 lg-xl:h-12 text-center mr-2 rounded-full transition-all duration-200 ease-linear hover:text-white hover:bg-[#0e0e0e]"
-        aria-label="GitHub"
-        href="https://github.com/prasanth2817"
-        target="_blank"
-      >
-        <FontAwesomeIcon icon={faGithub} />
-      </a>
-    </li>
-    <li className="inline-block mb-6 md:mb-8 lg:mb-10">
-      <a
-        className="Gmail inline-block text-2xl md:text-4xl lg-xl:text-3xl leading-[44px] md:leading-[58px] lg-xl:leading-[52px] text-cyan-400 bg-[#09011b] w-11 h-11 md:w-14 md:h-14 lg-xl:w-12 lg-xl:h-12 text-center mr-2 rounded-full transition-all duration-200 ease-linear hover:text-white hover:bg-[#00aced]"
-        aria-label="Mail"
-        href="mailto:prasanth2817ss@gmail.com"
-        target="_blank"
-      >
-        <FontAwesomeIcon icon={faEnvelope} />
-      </a>
-    </li>
-  </ul>
-</div>
 
+          <div className="socials relative mt-28 md:mt-36 lg:mt-40 xl:mt-44">
+            <ul className="social-icons space-x-4 p-0 mb-0 list-none">
+              <li className="inline-block mb-6 md:mb-8 lg:mb-10">
+                <a
+                  className="linkedin inline-block text-2xl md:text-4xl lg-xl:text-3xl leading-[44px] md:leading-[58px] lg-xl:leading-[52px] text-cyan-400 bg-[#09011b] w-11 h-11 md:w-14 md:h-14 lg-xl:w-12 lg-xl:h-12 text-center mr-2 rounded-full transition-all duration-200 ease-linear hover:text-white hover:bg-[#007bb6]"
+                  aria-label="LinkedIn"
+                  href="https://www.linkedin.com/in/prasanth2817"
+                  target="_blank"
+                >
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+              </li>
+              <li className="inline-block mb-6 md:mb-8 lg:mb-10">
+                <a
+                  className="github inline-block text-2xl md:text-4xl leading-[44px] lg-xl:text-3xl lg-xl:leading-[52px] md:leading-[58px] text-cyan-400 bg-[#09011b] w-11 h-11 md:w-14 md:h-14 lg-xl:w-12 lg-xl:h-12 text-center mr-2 rounded-full transition-all duration-200 ease-linear hover:text-white hover:bg-[#0e0e0e]"
+                  aria-label="GitHub"
+                  href="https://github.com/prasanth2817"
+                  target="_blank"
+                >
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              </li>
+              <li className="inline-block mb-6 md:mb-8 lg:mb-10">
+                <a
+                  className="Gmail inline-block text-2xl md:text-4xl lg-xl:text-3xl leading-[44px] md:leading-[58px] lg-xl:leading-[52px] text-cyan-400 bg-[#09011b] w-11 h-11 md:w-14 md:h-14 lg-xl:w-12 lg-xl:h-12 text-center mr-2 rounded-full transition-all duration-200 ease-linear hover:text-white hover:bg-[#00aced]"
+                  aria-label="Mail"
+                  href="mailto:prasanth2817ss@gmail.com"
+                  target="_blank"
+                >
+                  <FontAwesomeIcon icon={faEnvelope} />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
         <div className="container 1024x1366:order-1 1024x1366:top-20 mx-auto lg:mx-0 sm:ml-16 order-1 lg:order-2 w-10/12 xs:w-11/12 sm:w-3/4 lg:w-9/12 1024x1366:w-9/12 1024x1366:ml-36 1024x1366:mb-4 image relative lg:top-24 xl:ml-0 xl:mt-16">
           {darkMode ? (
